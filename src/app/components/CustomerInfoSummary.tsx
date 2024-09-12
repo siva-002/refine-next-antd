f21abe539b28e53fd3e0baffa1cae465332fffeb
siva-002 <vsivakumar971@gmail.com>
2024-09-12 14:39:59 +0530

    ant-d users-c

--- src/app/components/CustomerInfoSummary.tsx
+++ src/app/components/CustomerInfoSummary.tsx
@@ -5,7 +5,12 @@ import React from "react";
 const CustomerInfoSummary = ({ customer }: any) => {
   return (
     <Flex align="center" gap={32}>
-      <Avatar size={96} src={customer?.avatar?.[0]?.url} />
+      <Avatar
+        size={96}
+        src={
+          "https://a.storyblok.com/f/191576/1200x800/215e59568f/round_profil_picture_after_.webp"
+        }
+      />
       <Flex vertical>
         <Typography.Text type="secondary">#{customer?.id}</Typography.Text>
         <Typography.Title
