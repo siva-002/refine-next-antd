85e84bdffe27b488246ca124d5aec931fb8b1f07
siva-002 <vsivakumar971@gmail.com>
2024-09-12 11:28:24 +0530

    ant-d user-side

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,13 @@
 import { Suspense } from "react";
 import { Authenticated, WelcomePage } from "@refinedev/core";
 import { NavigateToResource } from "@refinedev/nextjs-router";
-import BlogPostList from "./Users/page";
+
 
 export default function IndexPage() {
   return (
     <Suspense>
        <Authenticated key="home-page" >
-        {/* <NavigateToResource /> */}
-        <BlogPostList/>
+        <NavigateToResource resource="users" />
       </Authenticated>
     </Suspense>
   );
