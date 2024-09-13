8053d0c13a9328493ac534fc2edddd9ae3aba702
siva-002 <vsivakumar971@gmail.com>
2024-09-13 11:46:54 +0530

    dropdown

--- src/app/components/OrderMenuButton.tsx
+++ src/app/components/OrderMenuButton.tsx
@@ -19,9 +19,17 @@ const OrderMenuButton = ({ status }: Istatus) => {
       key: 1,
       icon: <CheckCircleOutlined />,
       label: "Accept",
-      disabled: text == "Delivered" || text == "Cancelled" || text == "Ready",
+      disabled:
+        text == "Delivered" ||
+        text == "Cancelled" ||
+        text == "Ready" ||
+        text == "On The Way",
       style: {
         color: "green",
+        backgroundColor:
+          text == "Delivered" || text == "Cancelled" || text == "Ready"
+            ? "gray"
+            : "none",
       },
     },
     {
@@ -32,6 +40,10 @@ const OrderMenuButton = ({ status }: Istatus) => {
         text == "Cancelled" || text == "On The Way" || text == "Delivered",
       style: {
         color: "red",
+        backgroundColor:
+          text == "Cancelled" || text == "On The Way" || text == "Delivered"
+            ? "gray"
+            : "none",
       },
     },
   ];
