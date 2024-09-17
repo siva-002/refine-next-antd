d63f65e90820e98635f58803c50162a12987ac41
siva-002 <vsivakumar971@gmail.com>
2024-09-17 13:17:46 +0530

    map

--- src/app/components/showorder/Map.tsx
+++ src/app/components/showorder/Map.tsx
@@ -7,7 +7,12 @@ const MapComponent = () => {
   const position = [51.505, 86.25];
 
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer
+      center={[51.505, -0.09]}
+      zoom={13}
+      scrollWheelZoom={false}
+      style={{ width: "100vw", height: "100vh" }}
+    >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
