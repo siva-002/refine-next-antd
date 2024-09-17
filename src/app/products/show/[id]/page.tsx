863f78b5b83357adb09f9fd10c4cab29562f3bdb
Sabareesh G <sabareeshsoundar@gmail.com>
2024-09-17 11:45:12 +0530

    Product

---
+++ src/app/products/show/[id]/page.tsx
@@ -0,0 +1,8 @@
+import ProductDrawerShow from "@app/components/product/ProductDrawerShow";
+import React from "react";
+
+const ShowProduct = () => {
+  return <ProductDrawerShow />;
+};
+
+export default ShowProduct;
