201ceed8cce052e3b02beaf247071b850de949ed
siva-002 <vsivakumar971@gmail.com>
2024-09-17 14:45:39 +0530

    map

--- src/app/components/showorder/Map.tsx
+++ src/app/components/showorder/Map.tsx
@@ -2,7 +2,16 @@
 import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import { EnvironmentOutlined } from "@ant-design/icons";
+import L from "leaflet";
 
+const customIcon = new L.Icon({
+  iconUrl:
+    "https://png.pngtree.com/png-clipart/20230123/original/pngtree-flat-red-location-sign-png-image_8927579.png", // Replace with the path to your custom image
+  iconSize: [32, 32], // Size of the icon
+  iconAnchor: [16, 32], // Point of the icon which will correspond to marker's location
+  popupAnchor: [0, -32], // Point from which the popup should open relative to the iconAnchor
+});
 const MapComponent = () => {
   const position = [51.505, 86.25];
 
@@ -11,14 +20,14 @@ const MapComponent = () => {
       center={[51.505, -0.09]}
       zoom={13}
       scrollWheelZoom={false}
-      className="col-md-8"
+      className="col-md-12"
       style={{ height: "50vh" }}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
+      <Marker position={[51.505, -0.09]} icon={customIcon}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
