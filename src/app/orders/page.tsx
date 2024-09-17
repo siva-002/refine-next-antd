c72e636a39a1c0dee5696cdcad5d334e75be0d58
siva-002 <vsivakumar971@gmail.com>
2024-09-17 10:27:13 +0530

    orders-show

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -66,8 +66,8 @@ export default function UsersList() {
         {...tableProps}
         rowKey="id"
         onRow={(record: any) => {
-          console.log(record.id);
-          console.log(record);
+          // console.log(record.id);
+          // console.log(record);
           return {
             onClick: () => show("orders", record?.id),
           };
