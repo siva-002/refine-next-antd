2c1f9d6c8487b3279c4b5cc7ea82dd703adfc64e
siva-002 <vsivakumar971@gmail.com>
2024-09-17 13:26:00 +0530

    map

--- src/app/components/showorder/Map.tsx
+++ src/app/components/showorder/Map.tsx
@@ -11,7 +11,8 @@ const MapComponent = () => {
       center={[51.505, -0.09]}
       zoom={13}
       scrollWheelZoom={false}
-      style={{ width: "100%", height: "100%" }}
+      className="col-md-8"
+      style={{ height: "50vh" }}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
