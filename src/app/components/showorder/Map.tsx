82d7942ed108a5bec1bcd65644444f0ed5e0b4d9
siva-002 <vsivakumar971@gmail.com>
2024-09-17 13:06:17 +0530

    map

--- src/app/components/showorder/Map.tsx
+++ src/app/components/showorder/Map.tsx
@@ -1,32 +1,19 @@
 import React from "react";
-import Map, { NavigationControl } from "react-map-gl";
-import "mapbox-gl/dist/mapbox-gl.css"; // Add Mapbox CSS if using Mapbox styles
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 const MapComponent = () => {
-  const [viewport, setViewport] = React.useState({
-    latitude: 51.505,
-    longitude: -0.09,
-    zoom: 13,
-    bearing: 0,
-    pitch: 0,
-  });
-
-  // Function to handle viewport changes
-  const handleViewportChange = (nextViewport: any) => {
-    setViewport(nextViewport);
-  };
+  const position = [51.505, 86.25];
 
   return (
-    <Map
-      {...viewport}
-      style={{ width: "100vw", height: "100vh" }}
-      mapStyle="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" // OSM tiles
-      mapboxAccessToken="YOUR_MAPBOX_ACCESS_TOKEN" // Mapbox token
-      onMove={(evt) => handleViewportChange(evt.viewState)} // Updated event handler
-    >
-      <NavigationControl position="top-right" />{" "}
-      {/* Optional: Navigation controls */}
-    </Map>
+    <MapContainer bounds={position}>
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
   );
 };
 
