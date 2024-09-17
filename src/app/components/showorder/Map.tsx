c2737f016777151778ce8a8e95c771bb6669df60
siva-002 <vsivakumar971@gmail.com>
2024-09-17 15:33:44 +0530

    map

--- src/app/components/showorder/Map.tsx
+++ src/app/components/showorder/Map.tsx
@@ -10,6 +10,8 @@ const MapComponent = ({ data }: { data: IOrder | undefined | BaseRecord }) => {
   const position = data?.courier?.store?.address?.coordinate;
   console.log("dt", data);
   console.log("ps", position);
+  console.log(Number(position[0]));
+  console.log(Number(position[1]));
   //   const customIcon = new L.Icon({
   //     iconUrl:
   //       "https://png.pngtree.com/png-clipart/20230123/original/pngtree-flat-red-location-sign-png-image_8927579.png", // Replace with the path to your custom image
@@ -21,30 +23,25 @@ const MapComponent = ({ data }: { data: IOrder | undefined | BaseRecord }) => {
     iconUrl:
       "https://webstockreview.net/images/scooter-clipart-two-wheeler-2.png", // Replace with the path to your custom image
     iconSize: [32, 32], // Size of the icon
-    iconAnchor: [Number(position[0]), Number(position[1])], // Point of the icon which will correspond to marker's location
+    iconAnchor: position, // Point of the icon which will correspond to marker's location
     //   popupAnchor: [0, -32], // Point from which the popup should open relative to the iconAnchor
   });
   return (
-    position && (
-      <MapContainer
-        center={[Number(position[0]), Number(position[1])]}
-        zoom={13}
-        scrollWheelZoom={false}
-        className="col-md-12"
-        style={{ height: "50vh" }}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker
-          position={[Number(position[0]), Number(position[1])]}
-          icon={BikeIcon}
-        >
-          <Popup>{data?.courier?.store?.address?.text}</Popup>
-        </Marker>
-      </MapContainer>
-    )
+    <MapContainer
+      center={position}
+      zoom={13}
+      scrollWheelZoom={false}
+      className="col-md-12"
+      style={{ height: "50vh" }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position} icon={BikeIcon}>
+        <Popup>{data?.courier?.store?.address?.text}</Popup>
+      </Marker>
+    </MapContainer>
   );
 };
 
