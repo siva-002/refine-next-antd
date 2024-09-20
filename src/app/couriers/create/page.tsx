3be7395337d61c6c9d7bac2f9cda46210abb7190
siva-002 <vsivakumar971@gmail.com>
2024-09-20 14:59:55 +0530

    courier add

--- src/app/couriers/create/page.tsx
+++ src/app/couriers/create/page.tsx
@@ -2,9 +2,25 @@
 import { CloudUploadOutlined } from "@ant-design/icons";
 import { ICourier } from "@app/interfaces";
 import { Create, Edit, getValueFromEvent, useForm } from "@refinedev/antd";
-import { HttpError, useApiUrl, useSelect, useShow } from "@refinedev/core";
+import {
+  HttpError,
+  useApiUrl,
+  useNavigation,
+  useSelect,
+  useShow,
+} from "@refinedev/core";
 import type { BaseRecord } from "@refinedev/core";
-import { Avatar, Button, Flex, Form, Input, Select, Upload, theme } from "antd";
+import {
+  Avatar,
+  Button,
+  Drawer,
+  Flex,
+  Form,
+  Input,
+  Select,
+  Upload,
+  theme,
+} from "antd";
 export default function CreateCourier() {
   const apiUrl = useApiUrl();
   const { formProps, saveButtonProps, formLoading, onFinish } =
@@ -19,8 +35,9 @@ export default function CreateCourier() {
     optionValue: "id",
   });
   const { options } = useSelect({
-    resource: "stores"
+    resource: "stores",
   });
+  const { list } = useNavigation();
   // console.log("store", storeProps);
 
   //   for manually change values before updating and add onfinish attribute to form with this function
@@ -47,172 +64,174 @@ export default function CreateCourier() {
 
   return (
     <Create saveButtonProps={saveButtonProps}>
-      <Form
-        {...formProps}
-        // onFinish={handleSubmit}
-        labelCol={{ span: 4 }}
-        wrapperCol={{ span: 10 }}
-        labelAlign="left"
-      >
-        <Form.Item
-          name="images"
-          getValueFromEvent={getValueFromEvent}
-          // rules={[
-          //   {
-          //     required: true,
-          //   },
-          // ]}
+      <Drawer onClose={() => list("couriers")} open={true}>
+        <Form
+          {...formProps}
+          // onFinish={handleSubmit}
+          labelCol={{ span: 4 }}
+          wrapperCol={{ span: 10 }}
+          labelAlign="left"
         >
-          <Upload
-            action={`${apiUrl}/media/upload`}
-            // listType="picture-card"
-            // onChange={onChange}
-            // onPreview={onPreview}
-            showUploadList={false}
-            maxCount={1}
+          <Form.Item
+            name="images"
+            getValueFromEvent={getValueFromEvent}
+            // rules={[
+            //   {
+            //     required: true,
+            //   },
+            // ]}
           >
-            <Flex
-              vertical
-              align="center"
-              justify="center"
-              style={{
-                position: "relative",
-                height: "100%",
-              }}
+            <Upload
+              action={`${apiUrl}/media/upload`}
+              // listType="picture-card"
+              // onChange={onChange}
+              // onPreview={onPreview}
+              showUploadList={false}
+              maxCount={1}
             >
               <Flex
                 vertical
                 align="center"
                 justify="center"
                 style={{
-                  width: "200px",
-                  height: "200px",
-                  border: previewImageURL ? "" : "1px solid #44454770",
+                  position: "relative",
+                  height: "100%",
                 }}
-                className="position-relative d-flex align-items-center justify-content-center rounded-pill overflow-hidden p-0 m-0"
               >
-                <Avatar
-                  shape="square"
+                <Flex
+                  vertical
+                  align="center"
+                  justify="center"
                   style={{
-                    aspectRatio: 1,
-                    objectFit: "contain",
-                    width: previewImageURL ? "200px" : "100px",
-                    height: previewImageURL ? "200px" : "100px",
-                    marginTop: "auto",
-                    transform: previewImageURL ? "" : "translateY(-50%)",
+                    width: "200px",
+                    height: "200px",
+                    border: previewImageURL ? "" : "1px solid #44454770",
                   }}
-                  src={
-                    previewImageURL ||
-                    "https://img.icons8.com/?size=100&id=kq0iMadL2AjZ&format=png&color=393939"
-                  }
-                  alt="Product Image"
-                />
-                <div
-                  className="position-absolute d-flex align-items-center justify-content-center rounded-bottom p-1 fs-6"
-                  style={{
-                    bottom: "0",
-                    width: "100%",
-                    backgroundColor: token.volcano,
-                    color: "#fff",
-                    cursor: "pointer",
-                  }}
+                  className="position-relative d-flex align-items-center justify-content-center rounded-pill overflow-hidden p-0 m-0"
                 >
-                  <CloudUploadOutlined className="fs-4 px-2" />{" "}
-                  <span>{!previewImageURL ? "Upload" : "Change"}</span>
-                </div>
+                  <Avatar
+                    shape="square"
+                    style={{
+                      aspectRatio: 1,
+                      objectFit: "contain",
+                      width: previewImageURL ? "200px" : "100px",
+                      height: previewImageURL ? "200px" : "100px",
+                      marginTop: "auto",
+                      transform: previewImageURL ? "" : "translateY(-50%)",
+                    }}
+                    src={
+                      previewImageURL ||
+                      "https://img.icons8.com/?size=100&id=kq0iMadL2AjZ&format=png&color=393939"
+                    }
+                    alt="Product Image"
+                  />
+                  <div
+                    className="position-absolute d-flex align-items-center justify-content-center rounded-bottom p-1 fs-6"
+                    style={{
+                      bottom: "0",
+                      width: "100%",
+                      backgroundColor: token.volcano,
+                      color: "#fff",
+                      cursor: "pointer",
+                    }}
+                  >
+                    <CloudUploadOutlined className="fs-4 px-2" />{" "}
+                    <span>{!previewImageURL ? "Upload" : "Change"}</span>
+                  </div>
+                </Flex>
               </Flex>
-            </Flex>
-          </Upload>
-        </Form.Item>
-        <Form.Item
-          label={"Name"}
-          name="name"
-          rules={[
-            {
-              required: true,
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label={"Gsm"}
-          name="gsm"
-          rules={[
-            {
-              required: true,
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label={"Email"}
-          name="email"
-          rules={[
-            {
-              required: true,
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label={"Address"}
-          name="address"
-          rules={[
-            {
-              required: true,
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label={"Account Number"}
-          name="accountNumber"
-          rules={[
-            {
-              required: true,
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label={"Store"}
-          name={["store", "id"]}
-          rules={[
-            {
-              required: true,
-            },
-          ]}
-        >
-          <Select options={options} />
-        </Form.Item>
-        <Form.Item
-          label={"Vehicle"}
-          name={["vehicle", "id"]}
-          rules={[
-            {
-              required: true,
-            },
-          ]}
-        >
-          <Select {...vehicleProps} />
-        </Form.Item>
-        <Form.Item
-          label={"Vehicle Id"}
-          name="licensePlate"
-          rules={[
-            {
-              required: true,
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-      </Form>
+            </Upload>
+          </Form.Item>
+          <Form.Item
+            label={"Name"}
+            name="name"
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label={"Gsm"}
+            name="gsm"
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label={"Email"}
+            name="email"
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label={"Address"}
+            name="address"
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label={"Account Number"}
+            name="accountNumber"
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label={"Store"}
+            name={["store", "id"]}
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+          >
+            <Select options={options} />
+          </Form.Item>
+          <Form.Item
+            label={"Vehicle"}
+            name={["vehicle", "id"]}
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+          >
+            <Select {...vehicleProps} />
+          </Form.Item>
+          <Form.Item
+            label={"Vehicle Id"}
+            name="licensePlate"
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+        </Form>
+      </Drawer>
     </Create>
   );
 }
