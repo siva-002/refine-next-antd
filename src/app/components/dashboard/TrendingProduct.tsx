e21065e50e3b74dea17155b6a82673adaf053c6d
Sabareesh G <sabareeshsoundar@gmail.com>
2024-09-21 19:31:04 +0530

    Get Trending Data

--- src/app/components/dashboard/TrendingProduct.tsx
+++ src/app/components/dashboard/TrendingProduct.tsx
@@ -1,7 +1,32 @@
+import { ITrendingProducts } from "@app/interfaces";
+import { useSimpleList } from "@refinedev/antd";
+import { Flex, List } from "antd";
 import React from "react";
 
 const TrendingProduct = () => {
-  return <div>TrendingProduct</div>;
+  const { listProps } = useSimpleList<ITrendingProducts>({
+    resource: "trendingProducts",
+    pagination: { pageSize: 5, current: 1 },
+    syncWithLocation: false,
+  });
+  // console.log(listProps?.dataSource);
+  return (
+    <List
+      className="w-100"
+      {...listProps}
+      pagination={false}
+      bordered={false}
+      renderItem={(item, index) => {
+        return (
+          <Flex align="center" justify="space-between" className="w-100">
+            <span>Rank : {item.id}</span>
+            <span className="text-start w-50">{item.product.name}</span>
+            <span>Order Count : {item.orderCount}</span>
+          </Flex>
+        );
+      }}
+    />
+  );
 };
 
 export default TrendingProduct;
