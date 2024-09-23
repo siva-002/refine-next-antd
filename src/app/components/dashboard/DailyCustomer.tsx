ece4d564e6107f0c9b59505153149865efc3f23b
siva-002 <vsivakumar971@gmail.com>
2024-09-23 15:49:12 +0530

    dash

--- src/app/components/dashboard/DailyCustomer.tsx
+++ src/app/components/dashboard/DailyCustomer.tsx
@@ -20,7 +20,7 @@ const DailyCustomer = () => {
   });
 
   const plotdata =
-    customerData?.data?.map((item: any) => {
+    customerData?.data?.data?.map((item: any) => {
       return {
         time: item?.date?.unix(),
         timeText: item?.date?.format("DD MMM YYYY"),
@@ -28,17 +28,6 @@ const DailyCustomer = () => {
       };
     }) || [];
   console.log(customerData);
-  // const data = [
-  //   { year: "1991", value: 3 },
-  //   { year: "1992", value: 4 },
-  //   { year: "1993", value: 3.5 },
-  //   { year: "1994", value: 5 },
-  //   { year: "1995", value: 4.9 },
-  //   { year: "1996", value: 6 },
-  //   { year: "1997", value: 7 },
-  //   { year: "1998", value: 9 },
-  //   { year: "1999", value: 13 },
-  // ];
 
   const config = {
     plotdata,
