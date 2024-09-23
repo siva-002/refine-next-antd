99657515875d17c2968fc393808bdcc5f47b62d5
siva-002 <vsivakumar971@gmail.com>
2024-09-23 16:49:14 +0530

    dash

--- src/app/components/dashboard/DailyCustomer.tsx
+++ src/app/components/dashboard/DailyCustomer.tsx
@@ -18,24 +18,26 @@ const DailyCustomer = () => {
       query: dateWeekFilter,
     },
   });
-
-  const plotdata =
-    customerData?.data?.data?.map((item: any) => {
-      return {
-        time: item?.date?.unix(),
-        timeText: item?.date?.format("DD MMM YYYY"),
-        value: item?.value,
-      };
-    }) || [];
   console.log(customerData);
+  console.log(customerData?.data);
+  // const plotdata =
+  //   customerData?.data?.data?.map((item: any) => {
+  //     return {
+  //       time: item?.date?.unix(),
+  //       timeText: item?.date?.format("DD MMM YYYY"),
+  //       value: item?.value,
+  //     };
+  //   }) || [];
+  // console.log(customerData);
 
-  const config = {
-    plotdata,
-    xField: "timeText",
-    yField: "value",
-  };
+  // const config = {
+  //   plotdata,
+  //   xField: "timeText",
+  //   yField: "value",
+  // };
 
-  return plotdata ? <Bar {...config} /> : null;
+  return "";
+  // return plotdata ? <Bar {...config} /> : null;
 };
 
 export default DailyCustomer;
