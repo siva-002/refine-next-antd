d805022a8a8f95ac366acd1727ef9a9b6203f9f5
siva-002 <vsivakumar971@gmail.com>
2024-09-23 16:57:38 +0530

    dash

--- src/app/components/dashboard/DailyCustomer.tsx
+++ src/app/components/dashboard/DailyCustomer.tsx
@@ -22,24 +22,23 @@ const DailyCustomer = () => {
   if (customerData?.data) {
     console.log(customerData);
     console.log(customerData?.data?.data);
+    const plotdata =
+      customerData?.data?.data?.map((item: any) => {
+        return {
+          time: item?.date?.unix(),
+          timeText: item?.date?.format("DD MMM YYYY"),
+          value: item?.value,
+        };
+      }) || [];
+    const config = {
+      plotdata,
+      xField: "timeText",
+      yField: "value",
+    };
+    return <Bar {...config} />;
   }
-  // const plotdata =
-  //   customerData?.data?.data?.map((item: any) => {
-  //     return {
-  //       time: item?.date?.unix(),
-  //       timeText: item?.date?.format("DD MMM YYYY"),
-  //       value: item?.value,
-  //     };
-  //   }) || [];
-  // console.log(customerData);
 
-  // const config = {
-  //   plotdata,
-  //   xField: "timeText",
-  //   yField: "value",
-  // };
-
-  return "";
+  return null;
   // return plotdata ? <Bar {...config} /> : null;
 };
 
