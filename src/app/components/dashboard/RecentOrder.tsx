fd5ee6b901864da5332a23e1d74115cfd172d36b
siva-002 <vsivakumar971@gmail.com>
2024-09-23 12:53:59 +0530

    dashboard order

--- src/app/components/dashboard/RecentOrder.tsx
+++ src/app/components/dashboard/RecentOrder.tsx
@@ -1,6 +1,6 @@
 import { IOrder, IProduct } from "@app/interfaces";
 import { useList, useTable } from "@refinedev/core";
-import { Flex, List, Pagination, Typography } from "antd";
+import { Flex, List, Pagination, Spin, Typography } from "antd";
 import React, { useState } from "react";
 import CalculatePrice from "../CalculatePrice";
 import OrderMenuButton from "../OrderMenuButton";
@@ -8,14 +8,16 @@ import { getUniqueListWithCount } from "../getUniqueListWithCount";
 
 const GetProductList = ({ products }: { products: IProduct[] }) => {
   const data = getUniqueListWithCount({ list: products, field: "id" });
-  console.log(data);
-
-  return data?.map((item) => (
-    <Flex key={item?.id}>
-      <Typography.Text>{item?.name}</Typography.Text>
-      <Typography.Text type="secondary">x{item?.count}</Typography.Text>
-    </Flex>
-  ));
+  return (
+    <>
+      {data?.map((item) => (
+        <Flex key={item?.id}>
+          <Typography.Text>{item?.name}</Typography.Text>
+          <Typography.Text type="secondary"> x{item?.count}</Typography.Text>
+        </Flex>
+      ))}
+    </>
+  );
 };
 const RecentOrder = () => {
   const [pageSize, setPageSize] = useState<number>(8);
@@ -41,51 +43,66 @@ const RecentOrder = () => {
 
   return (
     <>
-      <List
-        style={{ width: "100%", position: "relative" }}
-        dataSource={OrdersData}
-        renderItem={(item) => {
-          return (
-            <List.Item>
-              <Flex style={{ width: "100%" }}>
-                <Typography.Text style={{ width: "15%" }}>
-                  #{item?.orderNumber}
-                </Typography.Text>
-                <Flex vertical={true} style={{ width: "40%" }}>
-                  <Typography.Text ellipsis>
-                    {item?.user?.fullName}
+      {isLoading ? (
+        <Spin
+          size="large"
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            width: "100%",
+            height: "100%",
+          }}
+        />
+      ) : (
+        <List
+          style={{ width: "100%", position: "relative" }}
+          dataSource={OrdersData}
+          renderItem={(item) => {
+            return (
+              <List.Item>
+                <Flex style={{ width: "100%" }}>
+                  <Typography.Text style={{ width: "15%" }}>
+                    #{item?.orderNumber}
                   </Typography.Text>
-                  <Typography.Text
-                    ellipsis
-                    style={{ width: "60%" }}
-                    type="secondary"
-                  >
-                    {item?.adress.text}
+                  <Flex vertical={true} style={{ width: "40%" }}>
+                    <Typography.Text ellipsis>
+                      {item?.user?.fullName}
+                    </Typography.Text>
+                    <Typography.Text
+                      ellipsis
+                      style={{ width: "80%" }}
+                      type="secondary"
+                    >
+                      {item?.adress.text}
+                    </Typography.Text>
+                  </Flex>
+                  <Flex vertical={true} style={{ width: "30%" }}>
+                    <GetProductList products={item?.products} />
+                    {/* <Typography.Text ellipsis></Typography.Text> */}
+                  </Flex>
+                  <Typography.Text style={{ width: "10%" }}>
+                    <CalculatePrice product={item?.products} />
                   </Typography.Text>
+                  <Flex style={{ width: "5%" }} justify="center">
+                    <OrderMenuButton record={item} />
+                  </Flex>
                 </Flex>
-                <Flex vertical={true} style={{ width: "30%" }}>
-                  <GetProductList products={item?.products} />
-                  {/* <Typography.Text ellipsis></Typography.Text> */}
-                </Flex>
-                <Typography.Text style={{ width: "10%" }}>
-                  <CalculatePrice product={item?.products} />
-                </Typography.Text>
-                <Flex style={{ width: "5%" }} justify="center">
-                  <OrderMenuButton record={item} />
-                </Flex>
-              </Flex>
-            </List.Item>
-          );
-        }}
-      />
-      <Pagination
-        style={{ position: "absolute", bottom: "20px", right: "10px" }}
-        current={currentPage}
-        pageSize={pageSize}
-        onChange={handlePageChange}
-        total={data?.total}
-        showSizeChanger={false}
-      />
+              </List.Item>
+            );
+          }}
+        />
+      )}
+      {OrdersData ? (
+        <Pagination
+          style={{ position: "absolute", bottom: "20px", right: "10px" }}
+          current={currentPage}
+          pageSize={pageSize}
+          onChange={handlePageChange}
+          total={data?.total}
+          showSizeChanger={false}
+        />
+      ) : null}
     </>
   );
 };
