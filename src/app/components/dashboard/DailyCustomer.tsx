4a25cc2bd8d65b938d378d0fe69da3d799c3e801
siva-002 <vsivakumar971@gmail.com>
2024-09-23 14:51:36 +0530

    dashboard

--- src/app/components/dashboard/DailyCustomer.tsx
+++ src/app/components/dashboard/DailyCustomer.tsx
@@ -1,7 +1,25 @@
 "use client";
 import React from "react";
 import { Bar } from "@ant-design/charts";
+import dayjs from "dayjs";
+import { useApiUrl, useCustom } from "@refinedev/core";
 const DailyCustomer = () => {
+  const today = dayjs();
+  const API_URL = useApiUrl();
+  const dateWeekFilter = {
+    start: today.subtract(6, "days").startOf("day").format(),
+    end: today.endOf("day").format(),
+  };
+
+  const { data: customerData } = useCustom({
+    url: `${API_URL}/newCustomers`,
+    method: "get",
+    config: {
+      query: dateWeekFilter,
+    },
+  });
+
+  console.log(customerData);
   const data = [
     { year: "1991", value: 3 },
     { year: "1992", value: 4 },
