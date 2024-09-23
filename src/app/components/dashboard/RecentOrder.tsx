0a46229521211f19528d638e52511d500e662bbb
siva-002 <vsivakumar971@gmail.com>
2024-09-23 10:15:40 +0530

    dashboard orders

--- src/app/components/dashboard/RecentOrder.tsx
+++ src/app/components/dashboard/RecentOrder.tsx
@@ -1,7 +1,55 @@
+import { IOrder } from "@app/interfaces";
+import { useList, useTable } from "@refinedev/core";
+import { Flex, List, Typography } from "antd";
 import React from "react";
+import CalculatePrice from "../CalculatePrice";
+import OrderMenuButton from "../OrderMenuButton";
 
 const RecentOrder = () => {
-  return <div>RecentOrder</div>;
+  const { data, isLoading } = useList<IOrder>({
+    resource: "orders",
+    sorters: [
+      {
+        field: "createdAt",
+        order: "desc",
+      },
+    ],
+    pagination: {
+      pageSize: 8,
+      current: 1,
+    },
+  });
+  const OrdersData = data?.data;
+
+  return (
+    <List
+      dataSource={OrdersData}
+      renderItem={(item) => {
+        return (
+          <List.Item>
+            <Flex>
+              <Typography.Title>#{item?.orderNumber}</Typography.Title>
+
+              <Flex vertical={true}>
+                <Typography.Text ellipsis>
+                  #{item?.user?.fullName}
+                </Typography.Text>
+                <Typography.Text ellipsis>
+                  #{item?.adress?.text}
+                </Typography.Text>
+              </Flex>
+              <Typography.Text>
+                <CalculatePrice product={item?.products} />
+              </Typography.Text>
+              <div>
+                <OrderMenuButton record={item} />
+              </div>
+            </Flex>
+          </List.Item>
+        );
+      }}
+    ></List>
+  );
 };
 
 export default RecentOrder;
